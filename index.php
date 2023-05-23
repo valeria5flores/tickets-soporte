783c1c738953d6b25249fd1e05d9f99072374b64
valeria <valeria@gmail.com>
2023-05-23 13:07:28 -0600

    My commit example.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title> SISTEMA DE TICKETS</title>
+  <title> SISTEMA DE TICKETS </title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
